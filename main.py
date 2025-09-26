de32106622b2980a0575f59d15198db62cd4ecd0
JpGamer000 <joao.lima.jesus@escola.pr.gov.br>
2025-09-26 07:43:50 -0300

    l

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from mysql.connector import (connection)
 from mysql.connector import errorcode
+import os
+from load_dotenv import load_dotenv
 
-bd_conexao = connection.MySQLConnection(
-    host='localhost',
-    user='root',
-    password='root',
-    database='bd_python'
-)
+load_dotenv()
+senha = os.getenv('senha_sql')
+
+try:
+    bd_conexao = connection.MySQLConnection(
+        host='localhost',
+        user='root',
+        password=senha,
+        database='bd_python'
+    )
+    print("Conexão bem sucedida!")
+
