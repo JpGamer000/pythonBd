886af91160ebd2dd4d25fa40955a639e2792af41
JpGamer000 <joao.lima.jesus@escola.pr.gov.br>
2025-09-26 08:38:15 -0300

    fernando lindo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mysql.connector import (connection)
 from mysql.connector import errorcode
 import os
-from load_dotenv import load_dotenv
+from dotenv import load_dotenv
 
 load_dotenv()
 senha = os.getenv('senha_sql')
@@ -14,4 +14,33 @@ try:
         database='bd_python'
     )
     print("Conexão bem sucedida!")
+except mysql.connector.Error as erro:  # Corrigido: adicionado 'mysql.connector' antes do Error
+    if erro.errno == errorcode.ER_BAD_DB_ERROR:
+        print("O banco de dados não existe!")
+    elif erro.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Usuário ou senha incorretos!")
+    else:
+        print(erro)
+        
+comando = bd_conexao.cursor()
+select = comando.execute("SELECT * FROM ALUNOS")
+resultado = comando.fetchall()
+for linha in resultado:
+   print(linha)
 
+sql_insert = "INSERT INTO ALUNOS (nome, ano) VALUES (%s, %s)"
+valores1 = ('Caio', '3 TEC')  
+valores2 = ('Bernardo', '3 TEC') 
+
+comando.execute(sql_insert, valores1)
+comando.execute(sql_insert, valores2)
+
+print("==TEST INSERT==")
+select = comando.execute("SELECT * FROM ALUNOS")
+resultado = comando.fetchall()
+for linha in resultado:
+   print(linha)
+
+comando.close()
+bd_conexao.commit()
+bd_conexao.close
